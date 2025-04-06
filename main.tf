f26b6409f3651108f2daf32327d1bb92b1da8813
AshuKPJ <41061708+AshuKPJ@users.noreply.github.com>
2025-04-06 23:06:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ filter {
 
 resource "aws_instance" "myec2" {
   ami    = data.aws_ami.myami.id 
-  instance_type = "t2.macro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Instance-2-cloud"
